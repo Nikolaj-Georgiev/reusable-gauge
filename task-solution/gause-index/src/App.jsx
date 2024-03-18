c764cc4ef72447cf6f0f13fc14c919f3e1c812c8
NGNikolay <georgiev.nikolaj@gmail.com>
2024-03-18 17:26:28 +0200

    changed the layout

--- task-solution/gause-index/src/App.jsx
+++ task-solution/gause-index/src/App.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import GaugeRevised from './components/GaugeRevised';
 
 function App() {
-  const [sizeValue, setSizeValue] = useState('');
+  const [sizeValue, setSizeValue] = useState('large');
   const [gaugeIndex, setGaugeIndex] = useState(0);
 
   const handleSubmit = (e) => {
@@ -18,6 +18,56 @@ function App() {
 
   return (
     <>
+      <div className='hero-container'>
+        <div className='row-1'>
+          <img
+            src='../0-index.jpg'
+            alt='0-index'
+          />
+          <img
+            src='../1-index.jpg'
+            alt='1-index'
+          />
+          <img
+            src='../2-index.jpg'
+            alt='2-index'
+          />
+          <img
+            src='../3-index.jpg'
+            alt='3-index'
+          />
+          <img
+            src='../4-index.jpg'
+            alt='4-index'
+          />
+          <img
+            src='../5-index.jpg'
+            alt='5-index'
+          />
+        </div>
+        <div className='row-2'>
+          <img
+            src='../6-index.jpg'
+            alt='6-index'
+          />
+          <img
+            src='../7-index.jpg'
+            alt='7-index'
+          />
+          <img
+            src='../8-index.jpg'
+            alt='8-index'
+          />
+          <img
+            src='../9-index.jpg'
+            alt='9-index'
+          />
+          <img
+            src='../10-index.jpg'
+            alt='10-index'
+          />
+        </div>
+      </div>
       <div className='container'>
         <GaugeRevised
           size={sizeValue}
